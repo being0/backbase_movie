2b8ecf9deeee729aed3df7d9313f08c024ef0b19
reza <raliakbari@gmail.com>
2022-04-02 16:15:02 +0200

    fix sonar issue

--- movie-rate-service/src/main/java/com/backbase/movie/rate/controller/error/RestExceptionHandler.java
+++ movie-rate-service/src/main/java/com/backbase/movie/rate/controller/error/RestExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import javax.validation.Path;
 import java.util.stream.Collectors;
@@ -28,8 +29,8 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<ErrorTo> handleConstraintViolationException(ConstraintViolationException e, HttpServletResponse response) {
         log.debug(e.getMessage());
 
-        String message = e.getConstraintViolations() == null ? "" : e.getConstraintViolations().stream()
-                .map(cv -> cv == null ? "null" : getLastPath(cv.getPropertyPath()) + ": " + cv.getMessage())
+        String message = e.getConstraintViolations().stream()
+                .map(this::constraintViolationToString)
                 .collect(Collectors.joining(", "));
 
         return new ResponseEntity<>(new ErrorTo(message), HttpStatus.BAD_REQUEST);
@@ -42,6 +43,9 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(new ErrorTo("An unexpected error occurred!"), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    private  String constraintViolationToString(ConstraintViolation cv) {
+        return cv == null ? "null" : getLastPath(cv.getPropertyPath()) + ": " + cv.getMessage();
+    }
 
     private String getLastPath(Path path) {
         String pathStr = String.valueOf(path);
